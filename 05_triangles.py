21609c65155e296ac1ac6bc2ff734ad7d1419880
Priscila <prisciladaniela25@hotmail.com>
2022-09-10 12:52:43 -0300

    triángulo

---
+++ 05_triangles.py
@@ -0,0 +1,34 @@
+import time
+# Este programa ayuda a saber si podemos hacer un triángulo
+# dadas tres longitudes
+
+print("Hola, te voy a ayudar con eso de los triángulos")
+l1= input("Ingresa la longitud uno\n") 
+l2= input("Ingresa la longitud dos\n") 
+l3= input("Ingresa la longitud tres\n")
+
+print("Las longitudes entregadas hasta el momento son:")
+
+print(l1,l2,l3)
+
+if(l1<= l2+l3):
+    print("Vamos bien, revisemos los otros  casos")
+    time.sleep(0.5)
+    if(l2<= l1+l3):
+       print("Seguimos  bien, vamos al tercero")
+       time.sleep(0.5)
+       if(l3<=l2+l1):
+          print("Si es posible un triángulo")
+       else:
+         print("No es posible el triángulo")
+    else:
+        print("No es posible el triángulo")
+else:
+ print("No es posible el triánggulo:",l1,"No es menor que",l2,"+", l3)            
+
+            
+
+
+   
+
+
